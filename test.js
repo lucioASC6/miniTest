6f1bd1e7ea01e727ad5672b891b2c18dfab850ff
lucioASC6 <dannyang0227@gmail.com>
2019-07-22 16:53:52 -0400

    complete

--- test.js
+++ test.js
@@ -59,8 +59,8 @@ let nestedArray = [
     [2,1],[5,8],[3,9]
 ]
 
-for (i = 0; i < 3; i++) {
-    for (j = 0, j < 2; j++) {
+for (i in nestedArray) {
+    for (j in nestedArray[i]) {
         console.log(nestedArray[i][j]);
     }
 }
